c8e8f4308f23b66ecb7ff4c4b20559d3e6c9c2cc
liangzhibin <buildgate@qq.com>
2023-07-16 17:35:43 +0800

    初始化demo

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,31 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/electron-vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
+import "./plugins/davinci.css";
+// import typescriptLogo from "./typescript.svg";
+// import viteLogo from "/electron-vite.svg";
+// import { setupCounter } from "./counter.ts";
+import { daVinci } from "./plugins/davinci.ts";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+// document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+//   <div>
+//     <a href="https://vitejs.dev" target="_blank">
+//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
+//     </a>
+//     <a href="https://www.typescriptlang.org/" target="_blank">
+//       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+//     </a>
+//     <h1>Vite + TypeScript</h1>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//     <p class="read-the-docs">
+//       Click on the Vite and TypeScript logos to learn more
+//     </p>
+//   </div>
+// `;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
 
-postMessage({ payload: 'removeLoading' }, '*')
+// postMessage({ payload: "removeLoading" }, "*");
+
+const test = new daVinci("#app");
+console.log(test);
--- src/plugins/davinci.ts
+++ src/plugins/davinci.ts
@@ -1,90 +1,89 @@
-//视觉层
-
-let davinci = document.createElement("canvas");
-let DToolbar = document.createElement("div");
-let container = document.createElement("div");
-let coordinateTip = document.createElement("div");
-let ctx = davinci.getContext("2d");
-let originElement = document.querySelector("#app");
-let isMousedown = false;
-let x = 0,
-  y = 0;
-let preX = 0,
-  preY = 0;
-
-//requestAnimationFrame节流
-function throttle(fn: (...rest: any) => any) {
-  let block = false;
-  return function (this: object) {
-    let self = this;
-    let args = arguments;
-    if (!block) {
-      block = true;
-      window.requestAnimationFrame(() => {
-        block = false;
-        fn.apply(self, args as any);
-      });
-    }
-  };
-}
-
-coordinateTip.className = "davinci_coordinateTip";
-container.className = "davinci_container";
-davinci.className = "davinci_body";
-DToolbar.className = "davinci_toolbar";
-
-let resizeWatcher = new ResizeObserver(() => {
-  davinci.width = (<HTMLDivElement>originElement).offsetWidth;
-  davinci.height = (<HTMLDivElement>originElement).offsetHeight;
-});
-resizeWatcher.observe(<HTMLDivElement>originElement);
-
-container.append(coordinateTip);
-container.append(davinci);
-container.append(DToolbar);
-originElement?.append(container);
-
-function draw(start: _type_coordinate, end: _type_coordinate) {
-  if (!isMousedown) {
-    return;
-  }
-  (<CanvasRenderingContext2D>ctx).beginPath();
-  (<CanvasRenderingContext2D>ctx).moveTo(start.x, start.y);
-  (<CanvasRenderingContext2D>ctx).lineTo(end.x, end.y);
-  (<CanvasRenderingContext2D>ctx).stroke();
-  preX = end.x;
-  preY = end.y;
-}
-
-davinci.addEventListener(
-  "mousemove",
-  throttle((e: MouseEvent) => {
-    coordinateTip.innerText = `${e.offsetX},${e.offsetY}`;
-    draw({ x: preX, y: preY }, { x: e.offsetX, y: e.offsetY });
-  })
-);
-
-davinci.addEventListener("mousedown", function (e: MouseEvent) {
-  isMousedown = true;
-  preX = e.offsetX;
-  preY = e.offsetY;
-});
-
-davinci.addEventListener("mouseup", function () {
-  isMousedown = false;
-});
-
-class daVinci {
+//
+export class daVinci {
   Dcanvas = document.createElement("canvas");
   DToolbar = document.createElement("div");
   Dcontainer = document.createElement("div");
   DcoordinateTip = document.createElement("div");
-  ctx = davinci.getContext("2d");
-  originElement = document.querySelector("#app");
-  isMousedown = false;
+  ctx = this.Dcanvas.getContext("2d");
+  originElement: Element | null = null;
+  painting = false;
   x = 0;
   y = 0;
   preX = 0;
   preY = 0;
-  constructor(fundElement: string) {}
+  resizeWatcher: ResizeObserver;
+
+  constructor(fundElement: string) {
+    let self = this;
+
+    this.originElement = document.querySelector(fundElement);
+    this.DcoordinateTip.className = "davinci_coordinateTip";
+    this.Dcontainer.className = "davinci_container";
+    this.Dcanvas.className = "davinci_body";
+    this.DToolbar.className = "davinci_toolbar";
+
+    //保证画布填满容器
+    this.resizeWatcher = new ResizeObserver(() => {
+      self.Dcanvas.width = (<HTMLDivElement>self.originElement).offsetWidth;
+      self.Dcanvas.height = (<HTMLDivElement>self.originElement).offsetHeight;
+    });
+    this.resizeWatcher.observe(<HTMLDivElement>self.originElement);
+
+    //鼠标监听
+    this.Dcanvas.addEventListener(
+      "mousemove",
+      this.throttle((e: MouseEvent) => {
+        self.DcoordinateTip.innerText = `${e.offsetX},${e.offsetY}`;
+        self.draw(
+          { x: self.preX, y: self.preY },
+          { x: e.offsetX, y: e.offsetY }
+        );
+      })
+    );
+
+    this.Dcanvas.addEventListener("mousedown", function (e: MouseEvent) {
+      self.painting = true;
+      self.preX = e.offsetX;
+      self.preY = e.offsetY;
+    });
+
+    this.Dcanvas.addEventListener("mouseup", function () {
+      self.painting = false;
+    });
+
+    //基础元素载入
+    this.Dcontainer.append(this.DcoordinateTip);
+    this.Dcontainer.append(this.Dcanvas);
+    this.Dcontainer.append(this.DToolbar);
+    this.originElement?.append(this.Dcontainer);
+  }
+
+  //节流
+  throttle(fn: (...rest: any) => any) {
+    let block = false;
+    return function (this: object) {
+      let self = this;
+      let args = arguments;
+      if (!block) {
+        block = true;
+        window.requestAnimationFrame(() => {
+          block = false;
+          fn.apply(self, args as any);
+        });
+      }
+    };
+  }
+
+  //绘画开始
+  draw(start: _type_coordinate, end: _type_coordinate) {
+    if (!this.painting) {
+      return;
+    }
+    (<CanvasRenderingContext2D>this.ctx).beginPath();
+    (<CanvasRenderingContext2D>this.ctx).moveTo(start.x, start.y);
+    (<CanvasRenderingContext2D>this.ctx).lineTo(end.x, end.y);
+    (<CanvasRenderingContext2D>this.ctx).stroke();
+    this.preX = end.x;
+    this.preY = end.y;
+  }
 }
