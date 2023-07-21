2a7103fcb6712ae025e0a6ec7b4e948d765cd713
liangzhibin <buildgate@qq.com>
2023-07-21 17:30:13 +0800

    0721

--- src/plugins/davinci.ts
+++ src/plugins/davinci.ts
@@ -2,9 +2,7 @@
 export class daVinci {
   //基础变量
   Dcanvas = document.createElement("canvas");
-  DToolbar = document.createElement("div");
   Dcontainer = document.createElement("div");
-  DcoordinateTip = document.createElement("div");
   ctx = this.Dcanvas.getContext("2d");
   originElement: Element | null = null;
   painting = false;
@@ -14,32 +12,46 @@ export class daVinci {
   preY = 0;
   resizeWatcher: ResizeObserver;
 
+  //附加项
+  coordinateTip: DcoordinateTip | null = null;
+
   //画像相关变量
   lineWidth = 1;
+  lineColor = "#000000";
+  lineCap: CanvasLineCap = "round";
+  mode = "custom";
 
-  constructor(fundElement: string) {
+  constructor(fundElement: string, options?: _type_options) {
     let self = this;
 
     this.originElement = document.querySelector(fundElement);
-    this.DcoordinateTip.className = "davinci_coordinateTip";
+
     this.Dcontainer.className = "davinci_container";
     this.Dcanvas.className = "davinci_body";
-    this.DToolbar.className = "davinci_toolbar";
 
     //保证画布填满容器
     this.resizeWatcher = new ResizeObserver(() => {
-      self.Dcanvas.width = (<HTMLDivElement>self.originElement).offsetWidth;
-      self.Dcanvas.height = (<HTMLDivElement>self.originElement).offsetHeight;
+      self.Dcanvas.width = (self.originElement as HTMLDivElement).offsetWidth;
+      self.Dcanvas.height = (self.originElement as HTMLDivElement).offsetHeight;
     });
-    this.resizeWatcher.observe(<HTMLDivElement>self.originElement);
+    this.resizeWatcher.observe(self.originElement as HTMLDivElement);
+
+    //选项初始化
+    this.setOptions(options);
 
     //鼠标监听
     this.Dcanvas.addEventListener(
       "mousemove",
       this.throttle((e: MouseEvent) => {
-        self.DcoordinateTip.innerText = `${e.offsetX},${e.offsetY}`;
+        if (self.coordinateTip) {
+          self.coordinateTip.coordinate = {
+            //若不整个对象赋值将不会出发setText函数
+            x: e.offsetX,
+            y: e.offsetY,
+          };
+        }
         if (this.painting) {
-          self.draw(
+          self.drawContinuous(
             { x: self.preX, y: self.preY },
             { x: e.offsetX, y: e.offsetY }
           );
@@ -55,16 +67,25 @@ export class daVinci {
       self.drawSpot({ x: e.offsetX, y: e.offsetY });
       self.preX = e.offsetX;
       self.preY = e.offsetY;
+      self.x = e.offsetX;
+      self.y = e.offsetY;
     });
 
-    this.Dcanvas.addEventListener("mouseup", function () {
+    this.Dcanvas.addEventListener("mouseup", function (e: MouseEvent) {
       self.painting = false;
+      if (self.mode === "straight" && self.ctx) {
+        self.ctx.beginPath();
+        self.ctx.lineWidth = self.lineWidth;
+        self.ctx.strokeStyle = self.lineColor;
+        self.ctx.lineCap = self.lineCap;
+        self.ctx.moveTo(self.x, self.y);
+        self.ctx.lineTo(e.offsetX, e.offsetY);
+        self.ctx.stroke();
+      }
     });
 
     //基础元素载入
-    this.Dcontainer.append(this.DcoordinateTip);
     this.Dcontainer.append(this.Dcanvas);
-    this.Dcontainer.append(this.DToolbar);
     this.originElement?.append(this.Dcontainer);
   }
 
@@ -84,26 +105,83 @@ export class daVinci {
     };
   }
 
-  //绘画开始,解决断点问题
-  draw(start: _type_coordinate, end: _type_coordinate) {
-    (<CanvasRenderingContext2D>this.ctx).lineWidth = this.lineWidth;
+  //连续线绘画,解决断点问题
+  drawContinuous(start: _type_coordinate, end: _type_coordinate) {
+    if (!this.ctx) {
+      return;
+    }
+    if (this.mode === "straight") {
+      return;
+    }
+    this.ctx.lineWidth = this.lineWidth;
 
-    (<CanvasRenderingContext2D>this.ctx).beginPath();
-    (<CanvasRenderingContext2D>this.ctx).moveTo(start.x, start.y);
-    (<CanvasRenderingContext2D>this.ctx).lineTo(end.x, end.y);
-    (<CanvasRenderingContext2D>this.ctx).stroke();
+    this.ctx.beginPath();
+    this.ctx.strokeStyle = this.lineColor;
+    this.ctx.lineCap = this.lineCap;
+    this.ctx.moveTo(start.x, start.y);
+    this.ctx.lineTo(end.x, end.y);
+
+    this.ctx.stroke();
   }
 
   //单点绘画
   drawSpot(position: _type_coordinate) {
-    (<CanvasRenderingContext2D>this.ctx).beginPath();
-    (<CanvasRenderingContext2D>this.ctx).arc(
-      position.x,
-      position.y,
-      this.lineWidth / 2,
-      0,
-      2 * Math.PI
-    );
-    (<CanvasRenderingContext2D>this.ctx).stroke();
+    if (!this.ctx) {
+      return;
+    }
+    this.ctx.beginPath();
+    this.ctx.arc(position.x, position.y, this.lineWidth / 2, 0, 2 * Math.PI);
+    this.ctx.fillStyle = this.lineColor;
+    this.ctx.fill();
+  }
+
+  //设置参数
+  setOptions(options?: _type_options) {
+    if (options?.coordinateTip) {
+      if (!this.coordinateTip) {
+        this.coordinateTip = new DcoordinateTip(this);
+        this.Dcontainer.append(this.coordinateTip.element);
+      }
+    } else {
+      if (options?.coordinateTip === false && this.coordinateTip) {
+        this.Dcontainer.removeChild(this.coordinateTip.element);
+      }
+    }
+    this.lineColor = options?.lineColor || this.lineColor;
+    this.lineWidth = options?.lineWidth || this.lineWidth;
+    this.lineCap = options?.lineCap || this.lineCap;
+    this.mode = options?.mode || this.mode;
+  }
+}
+
+//坐标提示类
+class DcoordinateTip {
+  Dentity: daVinci | null = null;
+  element = document.createElement("div");
+
+  coordinate: _type_coordinate = {
+    x: 0,
+    y: 0,
+  };
+
+  constructor(daVinci_entity: daVinci) {
+    let self = this;
+    if (!daVinci_entity) {
+      throw Error("未指定有效实体");
+    }
+    this.Dentity = daVinci_entity;
+    this.element.className = "davinci_coordinateTip";
+    return new Proxy(this, {
+      set: function (target, key, value, receiver) {
+        if (key === "coordinate") {
+          self.setText();
+        }
+        return Reflect.set(target, key, value, receiver);
+      },
+    });
+  }
+
+  setText() {
+    this.element.innerText = `${this.coordinate.x},${this.coordinate.y}`;
   }
 }
--- src/plugins/index.d.ts
+++ src/plugins/index.d.ts
@@ -2,3 +2,11 @@ declare interface _type_coordinate {
   x: number;
   y: number;
 }
+
+declare interface _type_options {
+  coordinateTip?: boolean;
+  lineColor?: string;
+  lineWidth?: number;
+  lineCap?: CanvasLineCap;
+  mode?: string;
+}
